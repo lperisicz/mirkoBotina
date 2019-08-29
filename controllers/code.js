5886fba8c60dc57649ec1dfb061f1ae13f20597f
Luka Perisic <luka.perisic@gauss.hr>
2019-08-29 16:06:21 +0200

    add code reckognition

--- controllers/code.js
+++ controllers/code.js
@@ -1,11 +1,23 @@
 const formatMessage = msg => {
-    msg.channel.send({
-            embed: {
-                title: msg.author.username,
-                description: "```bash\n" + msg.content.replace("!code ", "") + "```"
-            }
-        }
-    );
+
+    var Algorithmia = require("algorithmia");
+
+    var input = msg.content.replace("!code ", "");
+    Algorithmia.client("sim2jsCstf/qBpsI2Vei2wRbpo61")
+        .algo("PetiteProgrammer/ProgrammingLanguageIdentification/0.1.3?timeout=300") // timeout is optional
+        .pipe(input)
+        .then(function(response) {
+            console.log(response.get());
+            msg.channel.send({
+                    embed: {
+                        title: msg.author.username,
+                        description: response.get().toString()
+                    }
+                }
+            );
+        });
+
+
 };
 
 module.exports = {
