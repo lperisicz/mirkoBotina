441082eebb97696bb2f2dc64b2c199dd830e8ed9
Luka Perisic <luka.perisic@gauss.hr>
2019-08-29 20:22:41 +0200

    fix1 code formatter

--- controllers/code.js
+++ controllers/code.js
@@ -9,21 +9,19 @@ const formatMessage = msg => {
 
     var Algorithmia = require("algorithmia");
 
-    var input = msg.content.replace("!code ", "");
+    var input = prettier.format(msg.content.replace("!code ", ""));
     Algorithmia.client("sim2jsCstf/qBpsI2Vei2wRbpo61")
         .algo("PetiteProgrammer/ProgrammingLanguageIdentification/0.1.3?timeout=300") // timeout is optional
         .pipe(input)
         .then(function(response) {
             console.log(response.get());
 
-            let formattedCode = prettier.format(input);
-
-            //response.get().toString()
+            //.toString()
 
             msg.channel.send({
                     embed: {
                         title: msg.author.username,
-                        description: "```xml\n" + formattedCode + "```"
+                        description: "```" + `${response.get()[0][0]}\n` + input + "```"
                     }
                 }
             );
